fe5933e255799e5f628a1aab885cd311d3f10f7b
Sasha Raimov <crazyysasha@gmail.com>
2023-07-10 19:33:16 +0500

    fix modal

--- src/compoments/site/CModal.vue
+++ src/compoments/site/CModal.vue
@@ -2,27 +2,28 @@
   <div>
     <button @click="openModal"
       class="py-2 px-4 bg-gray-500 fixed top-0 right-0 mt-2 mr-3 text-white rounded-md">Вход</button>
-
-    <Transition name="modal"  enter-from-class="opacity-0 scale-50" enter-active-class="transition duration-200"  enter-to-class="opacity-100 scale-100" leave-active-class="opacity-0 scale-90">
-      <div v-if="showModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div class="bg-white p-6 rounded-md">
-          <form @submit.prevent="register">
-            <div class="mb-4">
-              <label class="block mb-2 text-sm font-bold" for="username">Имя пользователя</label>
-              <input v-model="username" type="text" id="username"
-                class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
-            </div>
-            <div class="mb-4">
-              <label class="block mb-2 text-sm font-bold" for="password">Пароль</label>
-              <input v-model="password" type="password" id="password"
-                class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
-            </div>
-            <button type="submit" class="py-2 px-4 bg-green-500 text-white rounded-md">Войти</button>
-          </form>
-          <button @click="closeModal" class="mt-4 py-2 px-4 bg-red-500 text-white rounded-md">Закрыть</button>
+    <Teleport to="body">
+      <Transition name="modal"  enter-from-class="opacity-0 scale-50" enter-active-class="transition duration-200"  enter-to-class="opacity-100 scale-100" leave-active-class="opacity-0 scale-90">
+        <div v-if="showModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div class="bg-white p-6 rounded-md">
+            <form @submit.prevent="register">
+              <div class="mb-4">
+                <label class="block mb-2 text-sm font-bold" for="username">Имя пользователя</label>
+                <input v-model="username" type="text" id="username"
+                  class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+              </div>
+              <div class="mb-4">
+                <label class="block mb-2 text-sm font-bold" for="password">Пароль</label>
+                <input v-model="password" type="password" id="password"
+                  class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+              </div>
+              <button type="submit" class="py-2 px-4 bg-green-500 text-white rounded-md">Войти</button>
+            </form>
+            <button @click="closeModal" class="mt-4 py-2 px-4 bg-red-500 text-white rounded-md">Закрыть</button>
+          </div>
         </div>
-      </div>
-    </Transition>
+      </Transition>
+    </Teleport>
   </div>
 </template>
 
