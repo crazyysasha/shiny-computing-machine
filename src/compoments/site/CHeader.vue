3f30b22bdcbb9ebae875e6b2e250358a9ece9151
lparamedicl <kondaurovr1@gmail.com>
2023-07-10 18:28:46 +0500

    пар

--- src/compoments/site/CHeader.vue
+++ src/compoments/site/CHeader.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { ref } from 'vue';
 
-const onScreen = () => {
-    
+const onScreen = ref(false)
+
+function screen (onScreen = (true)) {
+    document.getElementById("profile1").classList.remove("hidden")
 }
 
 </script>
@@ -21,7 +23,7 @@ const onScreen = () => {
         <div><DarkMode></DarkMode></div>
     </header>
 
-    <div class="absolute backdrop-blur-sm w-[100%] h-[100%] hidden">
+    <div class="absolute backdrop-blur-sm w-[100%] h-[100%] hidden" id="profile1">
         <div class="py-2 px-2 rounded-lg red-500 absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-180%] border border-black">Name <br> Surname <br> Number</div>
     </div>
 </template>
